ae79ca63503e33923c88d360b875663025d87c1e
曾賀岭 <bally@hopa.com.tw>
2017-12-04 13:46:57 +0800

    Initial Commit

---
+++ github/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  github
+//
+//  Created by Heling Tseng on 2017/12/4.
+//  Copyright © 2017年 Heling Tseng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ github/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  github
+//
+//  Created by Heling Tseng on 2017/12/4.
+//  Copyright © 2017年 Heling Tseng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
